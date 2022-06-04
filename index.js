8333add23b4258aa752369e2776e996bbe444240
Gustavo Alberto <gustavo.aew@gmail.com>
2022-06-04 12:29:31 -0300

    path

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const express = require("express");
 // const path = require('path');
 const database = require("./config/Database");
 const ControllerUser = require("controllers/User");
-const ControllerAuth = require("./controllers/Auth");
-const ControllerEvent = require("./controllers/Event");
+const ControllerAuth = require("controllers/Auth");
+const ControllerEvent = require("controllers/Event");
 
 database.connect();
 
