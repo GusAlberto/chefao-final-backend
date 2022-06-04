efb1c484505b569ccc30167924f12e5a140598cf
Gustavo Alberto <gustavo.aew@gmail.com>
2022-06-04 12:24:52 -0300

    import

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 require("dotenv").config();
 const express = require("express"); // const path = require('path');
 const database = require("./config/Database");
-const ControllerUser = require("./controllers/User");
+import { ControllerUser } from "./controllers/User";
+// const ControllerUser =   require("./controllers/User");
 const ControllerAuth = require("./controllers/Auth");
 const ControllerEvent = require("./controllers/Event");
 
